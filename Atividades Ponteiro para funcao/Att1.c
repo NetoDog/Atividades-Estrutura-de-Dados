4cd703cd78cd3df2d6a36abc1ef2a3bfe73f4a68
NetoDog <hilariocancineto@gmail.com>
2025-09-17 00:13:14 -0300

    up

--- Atividades Ponteiro para funcao/Att1.c
+++ Atividades Ponteiro para funcao/Att1.c
@@ -1,3 +1,15 @@
 #include <stdio.h>
 #include <math.h>
-#include "Utils.h"
+#include "C:\Users\Windows 10 pro\Documents\GitHub\Atividades-Estrutura-de-Dados\Utils.h"
+
+int main()
+{
+    void *a[2];
+
+    a[0] = alocaInt (5);
+    a[1] = alocaInt (4);
+    int *b = comparaInt(a[0],a[1]);
+    printf("catapimbas :%d",*b);
+    
+    return 0;
+}
