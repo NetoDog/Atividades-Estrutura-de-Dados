e595153a795b7034636a5710e1ef874126c55fbf
NetoDog <hilariocancineto@gmail.com>
2025-11-06 00:03:49 -0300

    up

--- EXERCICIOS FILA E PILHA/main2.c
+++ EXERCICIOS FILA E PILHA/main2.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "Utils.h"
+#include "C:\Users\Windows 10 pro\Documents\GitHub\Atividades-Estrutura-de-Dados\Ulilidades\Utils.h"
 #include "Pilha.h"
 #include "Fila.h"
-#include "Persistencia.h"
+//#include "Persistencia.h"
 
 int main(){
 
